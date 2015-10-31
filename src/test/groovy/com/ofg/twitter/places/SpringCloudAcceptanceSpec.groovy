48e8f94610518bc124610a6e42ba9abf4f5f8ec3
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-11-01 00:20:01 +0100

    Fixed for Travis

--- src/test/groovy/com/ofg/twitter/places/SpringCloudAcceptanceSpec.groovy
+++ src/test/groovy/com/ofg/twitter/places/SpringCloudAcceptanceSpec.groovy
@@ -1,10 +1,10 @@
 package com.ofg.twitter.places
-import com.ofg.config.BasicProfiles
+
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 
 @ContextConfiguration(classes = [ColleratorClientStubConfiguration])
-@ActiveProfiles(BasicProfiles.SPRING_CLOUD)
+@ActiveProfiles('springCloud')
 class SpringCloudAcceptanceSpec extends AbstractAcceptanceSpec {
 
 }
