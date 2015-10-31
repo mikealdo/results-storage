53ead63fd3c8876122bdf2efc57cdc17e1c3e26c
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-11-01 00:12:11 +0100

    One test not passing + app doesn't register properly

--- src/test/groovy/com/ofg/twitter/places/SpringCloudAcceptanceSpec.groovy
+++ src/test/groovy/com/ofg/twitter/places/SpringCloudAcceptanceSpec.groovy
@@ -1,10 +1,9 @@
 package com.ofg.twitter.places
-
 import com.ofg.config.BasicProfiles
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 
-@ContextConfiguration(classes = ColleratorClientStubConfiguration)
+@ContextConfiguration(classes = [ColleratorClientStubConfiguration])
 @ActiveProfiles(BasicProfiles.SPRING_CLOUD)
 class SpringCloudAcceptanceSpec extends AbstractAcceptanceSpec {
 
